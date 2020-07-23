8672676ca21626dee6d317ffee431ff33110168e
rockyl <rockyf@126.com>
2020-07-24 00:43:06 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { NativeModules } from 'react-native';
 
 type ChildprocessType = {
-  spawn(cmd: string, params?: any[], options?: any, stdoutCallback?: (string)=>void): Promise<number>;
+  spawn(cmd: string, params?: any[], options?: any, stdoutCallback?: (line:string)=>void): Promise<number>;
 };
 
 const { Childprocess } = NativeModules;

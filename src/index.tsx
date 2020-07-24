0ce224da0afae868251c84885663f787aae06b11
rockyl <rockyf@126.com>
2020-07-24 13:33:32 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,21 @@ import {NativeModules} from 'react-native';
 
 const Childprocess = NativeModules.Childprocess;
 
+Childprocess.addListener(
+	'stdout',
+	() => {
+		console.log(arguments);
+	}
+);
+
 export async function spawn(cmd: string, args?: string[], options?: any, stdout?: (output: string) => void, stderr?: (err: string) => void) {
 	const cmdId = await Childprocess.spawn(cmd, args, options);
-	Childprocess.addListener(
+	/*Childprocess.addListener(
 		'stdout',
 		({output, id}) => {
 			if(id === cmdId){
 				stdout && stdout(output);
 			}
 		}
-	);
+	);*/
 }
